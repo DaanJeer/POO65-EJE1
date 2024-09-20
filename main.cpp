e6dec2594906b3f556990036e7930841d9179eb4
Daniel M <danilex404@gmail.com>
2024-09-20 12:39:35 -0500

    Commit Inicial

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int deberes,trabajos,pruebas,examen;
+    cout<<"Ingrese la nota de deberes:"<<endl;
+    cin>>deberes;
+    cout<<"Ingrese la nota de trabajos:"<<endl;
+    cin>>trabajos;
+    cout<<"Ingrese la nota de pruebas:"<<endl;
+    cin>>pruebas;
+    cout<<"Ingrese la nota de examen:"<<endl;
+    cin>>examen;
+    if(deberes>10){
+        cout<<"Ingrese "
+    }
+
+    return 0;
+}
